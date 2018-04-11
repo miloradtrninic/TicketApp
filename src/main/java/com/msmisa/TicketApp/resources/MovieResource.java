3a76cc60ad9c10ea9f5b78d6f722fb304f51a3a4
Sara Corak <saracorak0210@gmail.com>
2018-04-11 02:28:14 +0200

    Popunjene resource klase za Theatre,Movie i Play

--- src/main/java/com/msmisa/TicketApp/resources/MovieResource.java
+++ src/main/java/com/msmisa/TicketApp/resources/MovieResource.java
@@ -1,12 +1,50 @@
 package com.msmisa.TicketApp.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.msmisa.TicketApp.beans.Movie;
+import com.msmisa.TicketApp.dto.DTO;
+import com.msmisa.TicketApp.dto.preview.MoviePreviewDTO;
 
 @RestController
 @RequestMapping(value="/movie")
 public class MovieResource extends AbstractController<Movie, Integer>{
 
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<MoviePreviewDTO>> getAll(){
+		List<Movie> list = getDao().getAll();
+		List<MoviePreviewDTO> movieDtoList = list.stream().map(c -> convertToDto(c, MoviePreviewDTO.class)).collect(Collectors.toList());
+		if(list.isEmpty())
+			return new ResponseEntity<List<MoviePreviewDTO>>(movieDtoList,HttpStatus.NO_CONTENT);
+		else
+			return new ResponseEntity<List<MoviePreviewDTO>>(movieDtoList,HttpStatus.OK);
+
+	}
+	
+	
+	@PostMapping(value="/new",
+			produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public MoviePreviewDTO addNew(@DTO(value=MoviePreviewDTO.class) Movie movie) {
+		return convertToDto(getDao().insert(movie), MoviePreviewDTO.class);
+	}
+	
+	
+	
+	@GetMapping(value="{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public MoviePreviewDTO getId (@PathVariable(value="id") Integer id) {
+		return convertToDto(getDao().get(id), MoviePreviewDTO.class);
+	}
+	
+	
 }
--- src/main/java/com/msmisa/TicketApp/resources/TheatreResource.java
+++ src/main/java/com/msmisa/TicketApp/resources/TheatreResource.java
@@ -1,12 +1,48 @@
 package com.msmisa.TicketApp.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.msmisa.TicketApp.beans.Theatre;
+import com.msmisa.TicketApp.dto.DTO;
+import com.msmisa.TicketApp.dto.preview.TheatrePreviewDTO;
 
 @RestController
 @RequestMapping(value="/theatre")
 public class TheatreResource extends AbstractController<Theatre, Integer> {
 
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<TheatrePreviewDTO>> getAll(){
+		List<Theatre> list = getDao().getAll();
+		List<TheatrePreviewDTO> theatreDtoList = list.stream().map(c -> convertToDto(c, TheatrePreviewDTO.class)).collect(Collectors.toList());
+		if(list.isEmpty())
+			return new ResponseEntity<List<TheatrePreviewDTO>>(theatreDtoList,HttpStatus.NO_CONTENT);
+		else
+			return new ResponseEntity<List<TheatrePreviewDTO>>(theatreDtoList,HttpStatus.OK);
+
+	}
+	
+	
+	@PostMapping(value="/new",
+			produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public TheatrePreviewDTO addNew(@DTO(value=TheatrePreviewDTO.class) Theatre theatre) {
+		return convertToDto(getDao().insert(theatre), TheatrePreviewDTO.class);
+	}
+	
+	
+	
+	@GetMapping(value="{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public TheatrePreviewDTO getId (@PathVariable(value="id") Integer id) {
+		return convertToDto(getDao().get(id), TheatrePreviewDTO.class);
+	}
+	
 }
