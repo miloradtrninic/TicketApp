9dc44f8b776959ea24fd3705d94dd80f4e24f17f
Sara Corak <saracorak0210@gmail.com>
2018-04-16 19:49:59 +0200

    Ispravak greske

---
+++ src/main/java/com/msmisa/TicketApp/dao/projection/TerminDao.java
@@ -0,0 +1,8 @@
+package com.msmisa.TicketApp.dao.projection;
+
+import com.msmisa.TicketApp.beans.Termin;
+import com.msmisa.TicketApp.dao.GenericDao;
+
+public interface TerminDao extends GenericDao<Termin, Integer> {
+
+}
